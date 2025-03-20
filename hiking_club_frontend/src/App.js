e1a8aa93cce72eeb6f9a810ff97bc126d72b6378
Kaveesha Kapuruge <kaveeshakapuruge@gmail.com>
2025-03-20 13:38:10 +0530

    hike details page added

--- hiking_club_frontend/src/App.js
+++ hiking_club_frontend/src/App.js
@@ -51,25 +51,32 @@ import Login from "./screens/Login";
 import Register from "./screens/Register";
 import AboutUs from "./screens/AboutUs"; 
 import Footer from "./components/Footer";
-import Header2 from "./components/Header2";
 
+// Admin Screens
 import AdminArticles from "./screens/AdminArticles";
 import AdminKnowledge from "./screens/AdminKnowledge";
 import AdminEvents from "./screens/AdminEvents";
 import AdminDashboard from "./screens/AdminDashboard";
 import AdminApplicants from "./screens/AdminApplicants";
 
+// New HikeDetails Component
+import HikeDetails from "./screens/HikeDetails";
 
+// Layout Component
 function Layout() {
   const location = useLocation();
   const isHomeScreen = location.pathname === "/";
 
   return (
     <>
+      {/* Conditionally render Header1 for HomeScreen and Header for other pages */}
       {isHomeScreen ? <Header1 /> : <Header />}
+
+      {/* Routes for all pages */}
       <Routes>
         <Route path="/" element={<Homescreen />} /> 
         <Route path="/hikes" element={<Hikes />} />
+        <Route path="/hike-details/:id" element={<HikeDetails />} /> {/* Add this route */}
         <Route path="/profile" element={<Profile />} />
         <Route path="/articles" element={<Articles />} />
         <Route path="/knowledge" element={<Knowledge />} />
@@ -77,20 +84,21 @@ function Layout() {
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<AboutUs />} />
 
-        <Route path="/AdminArticales" element={<AdminArticles />} />
+        {/* Admin Routes */}
+        <Route path="/AdminArticles" element={<AdminArticles />} />
         <Route path="/AdminKnowledge" element={<AdminKnowledge />} />
         <Route path="/AdminEvents" element={<AdminEvents />} />
         <Route path="/AdminDashboard" element={<AdminDashboard />} />
         <Route path="/AdminApplicants" element={<AdminApplicants />} />
-
       </Routes>
-      <div>
+
+      {/* Footer */}
       <Footer />
-      </div>
     </>
   );
 }
 
+// Main App Component
 function App() {
   return (
     <BrowserRouter>
